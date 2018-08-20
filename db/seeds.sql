5a8908222d8c4350892322c6f49e76903ae0a8d3
chriskelsey <chriskelsey.net@gmail.com>
2018-08-19 22:16:01 -0700

    Created SQL table and seed file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured) VALUES('Bacon Double Cheeseburger', FALSE);
+INSERT INTO burgers(burger_name, devoured) VALUES('Whopper', FALSE);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Big Mac', FALSE);
