6a8850f5827a54bd52ba66a3fabb1779e73c3ede
chriskelsey <chriskelsey.net@gmail.com>
2018-08-25 23:14:35 -0700

    Updating Heroku, clean up backend and some front end

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require('mysql');
+require('dotenv').config();
+var connection;
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-  	port: 3306,
-  	user: 'root',
-  	password: 'Delt@123',
-  	database: 'burgers_db'
-});
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+	connection = mysql.createConnection({
+		host: 'localhost',
+	  	port: 3306,
+	  	user: 'root',
+	  	password: process.env.MYSQL_ROOT_USER_PASSWORD,
+	  	database: 'burgers_db'
+	});
+}
 
 connection.connect(function(err) {
 	if(err){
